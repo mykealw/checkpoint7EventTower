07573914ca39683c0a1b5fbb6211d61bcb2b075f
Mykeal Wheeler <mykeal.wheeler@gmail.com>
2022-06-10 10:57:50 -0600

    push

---
+++ checkpoint7EventTower/client/assets/AboutPage.5c81aae5.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a9d18e7e.js";import{o,b as t,d as s}from"./vendor.95959a42.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,p,u,f,h){return o(),t("div",n,c)}var b=e(a,[["render",_]]);export{b as default};
